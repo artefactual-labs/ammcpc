ffa6d46b5082d93e6f57a7b417f1623347a6c6df
qubot <sysadmin@artefactual.com>
2019-03-15 10:59:00 -0700

    black: first run

--- ammcpc/ammcpc.py
+++ ammcpc/ammcpc.py
@@ -43,14 +43,14 @@ __version__ = "0.1.0"
 
 SUCCESS_CODE = 0
 ERROR_CODE = 1
-NS = '{https://mediaarea.net/mediaconch}'
+NS = "{https://mediaarea.net/mediaconch}"
 
 
 class MediaConchException(Exception):
     pass
 
 
-Parse = namedtuple('Parse', 'etree_el stdout')
+Parse = namedtuple("Parse", "etree_el stdout")
 
 
 class MediaConchPolicyCheckerCommand(object):
@@ -65,8 +65,7 @@ class MediaConchPolicyCheckerCommand(object):
         >>> checker.check('/path/to/file-to-be-checked')
     """
 
-    def __init__(self, policy_file_path=None, policy=None,
-                 policy_file_name=None):
+    def __init__(self, policy_file_path=None, policy=None, policy_file_name=None):
         self._policy_file_path = policy_file_path
         self._policy = policy
         self._policy_file_name = policy_file_name
@@ -82,16 +81,19 @@ class MediaConchPolicyCheckerCommand(object):
             self._validate()
             parse = self._parse_mediaconch_output(target)
             policy_checks = _get_policy_checks(parse.etree_el)
-            info, detail = self._get_evt_out_inf_detail(
-                policy_checks)
-            stdout = parse.stdout.decode('utf8')
-            print(json.dumps({
-                'eventOutcomeInformation': info,
-                'eventOutcomeDetailNote': detail,
-                'policy': self.policy,
-                'policyFileName': self.policy_file_name,
-                'stdout': stdout
-            }))
+            info, detail = self._get_evt_out_inf_detail(policy_checks)
+            stdout = parse.stdout.decode("utf8")
+            print(
+                json.dumps(
+                    {
+                        "eventOutcomeInformation": info,
+                        "eventOutcomeDetailNote": detail,
+                        "policy": self.policy,
+                        "policyFileName": self.policy_file_name,
+                        "stdout": stdout,
+                    }
+                )
+            )
             return SUCCESS_CODE
         except MediaConchException as exc:
             return self._error(exc)
@@ -100,8 +102,8 @@ class MediaConchPolicyCheckerCommand(object):
         if self._policy_file_path:
             if not os.path.isfile(self._policy_file_path):
                 raise MediaConchException(
-                    'There is no policy file at {}'.format(
-                        self._policy_file_path))
+                    "There is no policy file at {}".format(self._policy_file_path)
+                )
             self.policy_file_name = os.path.basename(self._policy_file_path)
             with open(self._policy_file_path) as filei:
                 self.policy = filei.read()
@@ -110,8 +112,9 @@ class MediaConchPolicyCheckerCommand(object):
             self.policy = self._policy
         else:
             raise MediaConchException(
-                'You must supply the path to a MediaConch policy file or'
-                ' the text of the policy and its name as strings.')
+                "You must supply the path to a MediaConch policy file or"
+                " the text of the policy and its name as strings."
+            )
 
     def _parse_mediaconch_output(self, target):
         """Run ``mediaconch -mc -fx -p <path_to_policy_xsl_file>
@@ -120,31 +123,38 @@ class MediaConchPolicyCheckerCommand(object):
         """
         try:
             if self._policy_file_path:
-                args = ['mediaconch', '-mc', '-fx', '-p',
-                        self._policy_file_path, target]
+                args = [
+                    "mediaconch",
+                    "-mc",
+                    "-fx",
+                    "-p",
+                    self._policy_file_path,
+                    target,
+                ]
                 output = subprocess.check_output(args)
             else:
                 ext = os.path.splitext(self.policy_file_name)[1]
                 pfp = tempfile.NamedTemporaryFile(suffix=ext, delete=False)
                 try:
-                    pfp.write(self._policy.encode('utf8'))
+                    pfp.write(self._policy.encode("utf8"))
                     pfp.close()
-                    args = ['mediaconch', '-mc', '-fx', '-p',
-                            pfp.name, target]
+                    args = ["mediaconch", "-mc", "-fx", "-p", pfp.name, target]
                     output = subprocess.check_output(args)
                 finally:
                     if not pfp.closed:
                         pfp.close()
                     os.unlink(pfp.name)
         except subprocess.CalledProcessError:
-            raise MediaConchException("MediaConch failed when running: %s" % (
-                ' '.join(args),))
+            raise MediaConchException(
+                "MediaConch failed when running: %s" % (" ".join(args),)
+            )
         try:
             return Parse(etree_el=etree.fromstring(output), stdout=output)
         except etree.XMLSyntaxError:
             raise MediaConchException(
                 "The MediaConch command failed when attempting to parse the"
-                " XML output by MediaConch:\n\n {}".format(output))
+                " XML output by MediaConch:\n\n {}".format(output)
+            )
 
     def _get_evt_out_inf_detail(self, policy_checks):
         """Return a 2-tuple of info and detail.
@@ -153,59 +163,66 @@ class MediaConchPolicyCheckerCommand(object):
         passed or failed. If the policy check as a whole passed, just return
         the passed check names; if it failed, just return the failed ones.
         """
-        if policy_checks['mc_version'] == '0.3':
-            return self._get_evt_out_inf_detail_v_0_3(
-                policy_checks)
+        if policy_checks["mc_version"] == "0.3":
+            return self._get_evt_out_inf_detail_v_0_3(policy_checks)
         return self._get_evt_out_inf_detail_v_0_1(policy_checks)
 
     def _get_evt_out_inf_detail_v_0_3(self, policy_checks):
         failed_policy_checks = set()
         passed_policy_checks = set()
-        info = 'fail'
-        if policy_checks['root_policy'][1] == 'pass':
-            info = 'pass'
-        for name, outcome in policy_checks['policies']:
+        info = "fail"
+        if policy_checks["root_policy"][1] == "pass":
+            info = "pass"
+        for name, outcome in policy_checks["policies"]:
             if outcome == "pass":
                 passed_policy_checks.add(name)
             else:
-                failed_policy_checks.add('failed policy/rule: %s' % name)
-        prefix = ('MediaConch policy check result against policy file'
-                  ' {}:'.format(self.policy_file_name))
-        if info == 'fail':
-            return ('fail', '{} {}'.format(
-                prefix, '; '.join(failed_policy_checks)))
+                failed_policy_checks.add("failed policy/rule: %s" % name)
+        prefix = "MediaConch policy check result against policy file" " {}:".format(
+            self.policy_file_name
+        )
+        if info == "fail":
+            return ("fail", "{} {}".format(prefix, "; ".join(failed_policy_checks)))
         if passed_policy_checks:
-            return ('pass', '{} All policy checks passed: {}'.format(
-                prefix, '; '.join(passed_policy_checks)))
-        return ('pass', '{} No checks passed, but none failed'
-                ' either.'.format(prefix))
+            return (
+                "pass",
+                "{} All policy checks passed: {}".format(
+                    prefix, "; ".join(passed_policy_checks)
+                ),
+            )
+        return (
+            "pass",
+            "{} No checks passed, but none failed" " either.".format(prefix),
+        )
 
     def _get_evt_out_inf_detail_v_0_1(self, policy_checks):
         failed_policy_checks = set()
         passed_policy_checks = set()
-        for name, (out, fie, act, rea) in policy_checks[
-                'policy_checks'].items():
+        for name, (out, fie, act, rea) in policy_checks["policy_checks"].items():
             if out == "pass":
                 passed_policy_checks.add(name)
             else:
                 failed_policy_checks.add(
                     'The check "{name}" failed; the actual value for the'
                     ' field "{fie}" was "{act}"; the reason was'
-                    ' "{rea}".'.format(
-                        name=name,
-                        fie=fie,
-                        act=act,
-                        rea=rea))
-        prefix = ('MediaConch policy check result against policy file'
-                  ' {}:'.format(self.policy_file_name))
+                    ' "{rea}".'.format(name=name, fie=fie, act=act, rea=rea)
+                )
+        prefix = "MediaConch policy check result against policy file" " {}:".format(
+            self.policy_file_name
+        )
         if failed_policy_checks:
-            return ('fail', '{} {}'.format(
-                prefix, ' '.join(failed_policy_checks)))
+            return ("fail", "{} {}".format(prefix, " ".join(failed_policy_checks)))
         if passed_policy_checks:
-            return ('pass', '{} All policy checks passed: {}'.format(
-                prefix, '; '.join(passed_policy_checks)))
-        return ('pass', '{} No checks passed, but none failed'
-                ' either.'.format(prefix))
+            return (
+                "pass",
+                "{} All policy checks passed: {}".format(
+                    prefix, "; ".join(passed_policy_checks)
+                ),
+            )
+        return (
+            "pass",
+            "{} No checks passed, but none failed" " either.".format(prefix),
+        )
 
     def _error(self, exc):
         try:
@@ -213,19 +230,23 @@ class MediaConchPolicyCheckerCommand(object):
             pol = self.policy
         except AttributeError:
             pfn = pol = None
-        print(json.dumps({
-            'eventOutcomeInformation': 'fail',
-            'eventOutcomeDetailNote': str(exc),
-            'policy': pol,
-            'policyFileName': pfn,
-            'stdout': None
-        }), file=sys.stderr)
+        print(
+            json.dumps(
+                {
+                    "eventOutcomeInformation": "fail",
+                    "eventOutcomeDetailNote": str(exc),
+                    "policy": pol,
+                    "policyFileName": pfn,
+                    "stdout": None,
+                }
+            ),
+            file=sys.stderr,
+        )
         return ERROR_CODE
 
 
 def _get_policy_check_name(policy_check_el):
-    return policy_check_el.attrib.get(
-        'name', 'Unnamed Check %s' % uuid.uuid4())
+    return policy_check_el.attrib.get("name", "Unnamed Check %s" % uuid.uuid4())
 
 
 def _parse_policy_check_test(policy_check_el):
@@ -237,47 +258,48 @@ def _parse_policy_check_test(policy_check_el):
     - El3 is the actual value of the relevant attribute/field.
     - El4 is the reason for the failure.
     """
-    test_el = policy_check_el.find('%stest' % NS)
+    test_el = policy_check_el.find("%stest" % NS)
     if test_el is None:
         return None
-    field = 'no field'
-    context_el = policy_check_el.find('%scontext' % NS)
+    field = "no field"
+    context_el = policy_check_el.find("%scontext" % NS)
     if context_el is not None:
-        field = context_el.attrib.get('field', 'no field')
+        field = context_el.attrib.get("field", "no field")
     return (
-        test_el.attrib.get('outcome', 'no outcome'),
+        test_el.attrib.get("outcome", "no outcome"),
         field,
-        test_el.attrib.get('actual', 'no actual value'),
-        test_el.attrib.get('reason', 'no reason')
+        test_el.attrib.get("actual", "no actual value"),
+        test_el.attrib.get("reason", "no reason"),
     )
 
 
 def _get_policy_checks_v_0_3(doc):
-    policy_checks = {'mc_version': '0.3', 'policies': []}
-    root_policy = doc.find('.%smedia/%spolicy' % (NS, NS))
+    policy_checks = {"mc_version": "0.3", "policies": []}
+    root_policy = doc.find(".%smedia/%spolicy" % (NS, NS))
     if root_policy is None:
-        raise MediaConchException('Unable to find a root policy')
-    policy_checks['root_policy'] = (
-        root_policy.attrib.get('name', 'No root policy name'),
-        root_policy.attrib.get('outcome', 'No root policy outcome')
+        raise MediaConchException("Unable to find a root policy")
+    policy_checks["root_policy"] = (
+        root_policy.attrib.get("name", "No root policy name"),
+        root_policy.attrib.get("outcome", "No root policy outcome"),
     )
-    for el_tname in ('policy', 'rule'):
-        path = './/%s%s' % (NS, el_tname)
+    for el_tname in ("policy", "rule"):
+        path = ".//%s%s" % (NS, el_tname)
         for policy_el in doc.iterfind(path):
             policy_name = _get_policy_check_name(policy_el)
-            policy_checks['policies'].append(
-                (policy_name, policy_el.attrib.get('outcome', 'no outcome')))
+            policy_checks["policies"].append(
+                (policy_name, policy_el.attrib.get("outcome", "no outcome"))
+            )
     return policy_checks
 
 
 def _get_policy_checks_v_0_1(doc):
-    policy_checks = {'mc_version': '0.1', 'policy_checks': {}}
-    path = '.%smedia/%spolicyChecks/%scheck' % (NS, NS, NS)
+    policy_checks = {"mc_version": "0.1", "policy_checks": {}}
+    path = ".%smedia/%spolicyChecks/%scheck" % (NS, NS, NS)
     for policy_check_el in doc.iterfind(path):
         policy_check_name = _get_policy_check_name(policy_check_el)
         parse = _parse_policy_check_test(policy_check_el)
         if parse:
-            policy_checks['policy_checks'][policy_check_name] = parse
+            policy_checks["policy_checks"][policy_check_name] = parse
     return policy_checks
 
 
@@ -285,21 +307,20 @@ def _get_policy_checks(doc):
     """Get all of the policy check names and outcomes from the policy check
     output file parsed as ``doc``.
     """
-    mc_xml_vrsn = doc.attrib.get('version',
-                                 'No identifiable MediaConch XML version')
-    if mc_xml_vrsn == '0.3':
+    mc_xml_vrsn = doc.attrib.get("version", "No identifiable MediaConch XML version")
+    if mc_xml_vrsn == "0.3":
         return _get_policy_checks_v_0_3(doc)
-    elif mc_xml_vrsn == '0.1':
+    elif mc_xml_vrsn == "0.1":
         return _get_policy_checks_v_0_1(doc)
     else:
         raise MediaConchException(
-            'Unable to parse MediaConch XML files with version'
-            ' "{}"'.format(mc_xml_vrsn))
+            "Unable to parse MediaConch XML files with version"
+            ' "{}"'.format(mc_xml_vrsn)
+        )
 
 
 def main():
     target = sys.argv[1]
     policy = sys.argv[2]
-    policy_checker = MediaConchPolicyCheckerCommand(
-        policy_file_path=policy)
+    policy_checker = MediaConchPolicyCheckerCommand(policy_file_path=policy)
     sys.exit(policy_checker.check(target))
--- test/test_ammcpc.py
+++ test/test_ammcpc.py
@@ -3,6 +3,7 @@ import json
 import os
 import sys
 from unittest import TestCase
+
 try:
     from cStringIO import StringIO
 except ImportError:
@@ -16,12 +17,12 @@ HERE = os.path.dirname(os.path.realpath(__file__))
 # behave the same. At some point MediaConch switched over to the more readable
 # .xml policy file format but retaned backwards-compatible support for the .xsl
 # format.
-POLICY_XSL_NAME = 'NYULib_MKVFFV1_MODIFIED.xsl'
-POLICY_XSL_PATH = os.path.join(HERE, 'policies', POLICY_XSL_NAME)
-POLICY_XML_NAME = 'NYULib_MKVFFV1_MODIFIED.xml'
-POLICY_XML_PATH = os.path.join(HERE, 'policies', POLICY_XML_NAME)
-FILE_FAILS_PATH = os.path.join(HERE, 'files', 'fails.mkv')
-FILE_PASSES_PATH = os.path.join(HERE, 'files', 'passes.mkv')
+POLICY_XSL_NAME = "NYULib_MKVFFV1_MODIFIED.xsl"
+POLICY_XSL_PATH = os.path.join(HERE, "policies", POLICY_XSL_NAME)
+POLICY_XML_NAME = "NYULib_MKVFFV1_MODIFIED.xml"
+POLICY_XML_PATH = os.path.join(HERE, "policies", POLICY_XML_NAME)
+FILE_FAILS_PATH = os.path.join(HERE, "files", "fails.mkv")
+FILE_PASSES_PATH = os.path.join(HERE, "files", "passes.mkv")
 
 
 class Capturing(list):
@@ -56,23 +57,25 @@ class TestMediaConchPolicyCheckerCommand(TestCase):
         attribute whose value is 'fail'.
         """
         policy_checker = MediaConchPolicyCheckerCommand(
-            policy_file_path=POLICY_XSL_PATH)
+            policy_file_path=POLICY_XSL_PATH
+        )
         with Capturing() as output:
             exitcode = policy_checker.check(FILE_FAILS_PATH)
         output = json.loads(output[0])
         assert exitcode == 0
-        assert output['eventOutcomeInformation'] == 'fail'
+        assert output["eventOutcomeInformation"] == "fail"
         with open(POLICY_XSL_PATH) as filei:
-            assert output['policy'] == filei.read()
+            assert output["policy"] == filei.read()
         policy_checker = MediaConchPolicyCheckerCommand(
-            policy_file_path=POLICY_XML_PATH)
+            policy_file_path=POLICY_XML_PATH
+        )
         with Capturing() as output:
             exitcode = policy_checker.check(FILE_FAILS_PATH)
         output = json.loads(output[0])
         assert exitcode == 0
-        assert output['eventOutcomeInformation'] == 'fail'
+        assert output["eventOutcomeInformation"] == "fail"
         with open(POLICY_XML_PATH) as filei:
-            assert output['policy'] == filei.read()
+            assert output["policy"] == filei.read()
 
     def test_check_bad_file_str_pol(self):
         """Same as ``test_check_bad_file`` except that the policy is passed as
@@ -81,23 +84,25 @@ class TestMediaConchPolicyCheckerCommand(TestCase):
         with open(POLICY_XSL_PATH) as filei:
             policy = filei.read()
         policy_checker = MediaConchPolicyCheckerCommand(
-            policy=policy, policy_file_name=POLICY_XSL_NAME)
+            policy=policy, policy_file_name=POLICY_XSL_NAME
+        )
         with Capturing() as output:
             exitcode = policy_checker.check(FILE_FAILS_PATH)
         output = json.loads(output[0])
         assert exitcode == 0
-        assert output['eventOutcomeInformation'] == 'fail'
-        assert output['policy'] == policy
+        assert output["eventOutcomeInformation"] == "fail"
+        assert output["policy"] == policy
         with open(POLICY_XML_PATH) as filei:
             policy = filei.read()
         policy_checker = MediaConchPolicyCheckerCommand(
-            policy=policy, policy_file_name=POLICY_XML_NAME)
+            policy=policy, policy_file_name=POLICY_XML_NAME
+        )
         with Capturing() as output:
             exitcode = policy_checker.check(FILE_FAILS_PATH)
         output = json.loads(output[0])
         assert exitcode == 0
-        assert output['eventOutcomeInformation'] == 'fail'
-        assert output['policy'] == policy
+        assert output["eventOutcomeInformation"] == "fail"
+        assert output["policy"] == policy
 
     def test_check_good_file(self):
         """Expect a policy check on a passing file to return a 0 exit code and
@@ -105,23 +110,25 @@ class TestMediaConchPolicyCheckerCommand(TestCase):
         attribute whose value is 'pass'.
         """
         policy_checker = MediaConchPolicyCheckerCommand(
-            policy_file_path=POLICY_XSL_PATH)
+            policy_file_path=POLICY_XSL_PATH
+        )
         with Capturing() as output:
             exitcode = policy_checker.check(FILE_PASSES_PATH)
         output = json.loads(output[0])
         assert exitcode == 0
-        assert output['eventOutcomeInformation'] == 'pass'
+        assert output["eventOutcomeInformation"] == "pass"
         with open(POLICY_XSL_PATH) as filei:
-            assert output['policy'] == filei.read()
+            assert output["policy"] == filei.read()
         policy_checker = MediaConchPolicyCheckerCommand(
-            policy_file_path=POLICY_XML_PATH)
+            policy_file_path=POLICY_XML_PATH
+        )
         with Capturing() as output:
             exitcode = policy_checker.check(FILE_PASSES_PATH)
         output = json.loads(output[0])
         assert exitcode == 0
-        assert output['eventOutcomeInformation'] == 'pass'
+        assert output["eventOutcomeInformation"] == "pass"
         with open(POLICY_XML_PATH) as filei:
-            assert output['policy'] == filei.read()
+            assert output["policy"] == filei.read()
 
     def test_check_good_file_str_pol(self):
         """Same as ``test_check_good_file`` except that the policy is passed as
@@ -130,34 +137,39 @@ class TestMediaConchPolicyCheckerCommand(TestCase):
         with open(POLICY_XSL_PATH) as filei:
             policy = filei.read()
         policy_checker = MediaConchPolicyCheckerCommand(
-            policy=policy, policy_file_name=POLICY_XSL_NAME)
+            policy=policy, policy_file_name=POLICY_XSL_NAME
+        )
         with Capturing() as output:
             exitcode = policy_checker.check(FILE_PASSES_PATH)
         output = json.loads(output[0])
         assert exitcode == 0
-        assert output['eventOutcomeInformation'] == 'pass'
-        assert output['policy'] == policy
+        assert output["eventOutcomeInformation"] == "pass"
+        assert output["policy"] == policy
         with open(POLICY_XML_PATH) as filei:
             policy = filei.read()
         policy_checker = MediaConchPolicyCheckerCommand(
-            policy=policy, policy_file_name=POLICY_XML_NAME)
+            policy=policy, policy_file_name=POLICY_XML_NAME
+        )
         with Capturing() as output:
             exitcode = policy_checker.check(FILE_PASSES_PATH)
         output = json.loads(output[0])
         assert exitcode == 0
-        assert output['eventOutcomeInformation'] == 'pass'
-        assert output['policy'] == policy
+        assert output["eventOutcomeInformation"] == "pass"
+        assert output["policy"] == policy
 
     def test_no_policy(self):
         """Expect a 1 exit code and a fail outcome when the policy file does
         not exist.
         """
         policy_checker = MediaConchPolicyCheckerCommand(
-            policy_file_path='fake/policy/path')
+            policy_file_path="fake/policy/path"
+        )
         with Capturing() as output:
             exitcode = policy_checker.check(FILE_PASSES_PATH)
         output = json.loads(output[0])
         assert exitcode == 1
-        assert output['eventOutcomeInformation'] == 'fail'
-        assert output['eventOutcomeDetailNote'] == \
-            'There is no policy file at fake/policy/path'
+        assert output["eventOutcomeInformation"] == "fail"
+        assert (
+            output["eventOutcomeDetailNote"]
+            == "There is no policy file at fake/policy/path"
+        )
